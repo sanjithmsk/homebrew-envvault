cecdd6e97f56dd395de8931491080dd425f12057
muhammed sanjith <46193060+sanjithmsk@users.noreply.github.com>
2024-12-19 14:33:23 +0530

    Update envvault.rb
    sha changed

--- Formula/envvault.rb
+++ Formula/envvault.rb
@@ -2,7 +2,7 @@ class Envvault < Formula
     desc "Secure, scalable, and collaborative environment variable management"
     homepage "https://envvault.com/"
     url "https://envvault.com/releases/envvault-0.0.2.tar.gz"
-    sha256 "ab305c220721c4cb8be0db5420824f97b6c98ba1a6435875618a348ba9370fbc"
+    sha256 "73b7b8eb54d5f7daf6e17d27803de4079e648c05811a577e47528470ac6bbe2f"
     license "MIT"
   
     depends_on "libtool" => :build
